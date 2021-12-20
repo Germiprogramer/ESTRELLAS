b37188ef2ca671271e41d27fe3334fc538e4ccfb
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-20 22:42:33 +0100

    no he conseguido que funcione con input

--- estrellitas.py
+++ estrellitas.py
@@ -1,13 +1,14 @@
 #tan solo me deja modificar el numero de puntas desde el programa, desgraciadamente no se porque no carga con int(input)
-n = 9
+n =9
 from turtle import *
-color("red","white")
 
-
-if n>5:
+def estrella(n):
+    color("red","white")
     setposition(-150,0)
-    while True:
+    while n>4:
         forward(300)
         left((360/n)*5)
         if abs(pos())< 1:
             break
+
+estrella(n)
