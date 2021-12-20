4aca498a66c76db9771ab40e99f898e9bdb7aa55
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-20 22:20:50 +0100

    "solucionado" el problema del angulo

--- estrellitas.py
+++ estrellitas.py
@@ -1,9 +1,13 @@
+#tan solo me deja modificar el numero de puntas desde el programa, desgraciadamente no se porque no carga con int(input)
+n = 9
 from turtle import *
 color("red","white")
 
-setposition(-150,0)
-while True:
-    forward(300)
-    left(170)
-    if abs(pos())< 1:
-        break
+
+if n>5:
+    setposition(-150,0)
+    while True:
+        forward(300)
+        left((360/n)*5)
+        if abs(pos())< 1:
+            break
